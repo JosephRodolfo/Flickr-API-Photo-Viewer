b2e69a55c87c17efa18590cf4ccfd1d827238382
JosephRodolfo <38168806+JosephRodolfo@users.noreply.github.com>
2022-03-06 11:32:11 -0500

    Add text input and putton to switch users to see other users photos

--- app.js
+++ app.js
@@ -2,25 +2,30 @@ var apiKey = config.YOUR_API_KEY;
 
 let currentPageNumber = {
   page: 1,
+  currentUserId: '75430200%40N04'
 };
 
 //async function, takes the desired page number returns list of photos with information needed to create src to display them,
 // currently set to only 10, but remove slice
 //and it would be all photos per page, 100;
 
-async function getImageList(pageNumber) {
+async function getImageList(pageNumber, userIdNumber) {
   const response = await fetch(
     "https://www.flickr.com/services/rest/?method=flickr.people.getPhotos&api_key=" +
       apiKey +
-      "&user_id=75430200%40N04&page=" +
+      "&user_id=" + userIdNumber + "&page=" + //75430200%40N04
       pageNumber +
       "&format=json&nojsoncallback=1"
   );
   const flickrObjectResponse = await response.json();
-  //   console.log(flickrObjectResponse.photos.photo.length);
-  printPageNumbers(flickrObjectResponse.photos.pages);
+  /*I need to have this return an object with the pages value and the list of photos json object to seperate this
+  printPageNumbers function from this and have it in a .then function */
+  printPageNumbers(flickrObjectResponse.photos.pages, userIdNumber);
 
   let sliced = flickrObjectResponse.photos.photo.slice(0, 10);
+  
+  
+  
   return sliced;
 }
 
@@ -39,12 +44,13 @@ function createAndAppend(imgSrc) {
   imageContainer.appendChild(imgHolder);
 }
 
-//function that is called when pressing arrow buttons or number buttons to select or move between pages.
-function updateImages(pageNumberInput) {
+//function that is called when pressing arrow buttons or number buttons to select or move between pages and when 
+//starting program
+function updateImages(pageNumberInput, userId) {
   let gridImageContainer = document.getElementById("grid-image-container");
   gridImageContainer.innerHTML = "";
 
-  getImageList(pageNumberInput)
+  getImageList(pageNumberInput, userId)
     .then((value) =>
       value.forEach((element) => {
         let tempSrc = createImgSrc(element.server, element.id, element.secret);
@@ -63,17 +69,19 @@ function updateImages(pageNumberInput) {
     );
 }
 
+
+//takes number of pages of photos and creates and appends the individual numbers to click on to visit other pages
 function printPageNumbers(numberOfPages) {
   let numbersContainer = document.getElementById("page-numbers-container");
   numbersContainer.innerHTML = "";
-  for (let i = 0; i < numberOfPages; i++) {
+  for (let i = 1; i < numberOfPages; i++) {
     let tempNumberHolder = document.createElement("div");
     tempNumberHolder.innerText = i;
     tempNumberHolder.id = i;
 
     tempNumberHolder.addEventListener("click", function () {
       currentPageNumber.page = this.innerText;
-      updateImages(this.innerText);
+      updateImages(this.innerText, currentPageNumber.currentUserId);
       console.log(this.innerText);
     });
 
@@ -81,39 +89,31 @@ function printPageNumbers(numberOfPages) {
   }
 }
 
+/*Adds event listners to the arrow buttons and the change user id buttons*/ 
 function setUpButtons() {
   let buttons = document.querySelectorAll("button");
 
   buttons.forEach((element) => {
+    if (element.id === "left-button") {
+      element.addEventListener("click", function () {
+        currentPageNumber.page--;
+        updateImages(currentPageNumber.page, currentPageNumber.currentUserId);
+      });
+    } else if (element.id === "right-button") {
+      element.addEventListener("click", function () {
+        currentPageNumber.page++;
+        updateImages(currentPageNumber.page, currentPageNumber.currentUserId);
+      });
+    } else {
 
+      element.addEventListener('click', function () {
 
-    if(element.id==="left-button"){
-    element.addEventListener("click", function () {
+         currentPageNumber.currentUserId = document.querySelector("input").value;
+        
+        updateImages(1, currentPageNumber.currentUserId);
 
-      currentPageNumber.page--;
-      updateImages(currentPageNumber.page);
-  });} else {
-
-
-    
-    element.addEventListener("click", function () {
-
-
-      currentPageNumber.page++;
-      updateImages(currentPageNumber.page);
-
-
-    })
-
-
-
-
-
-    
-  }
-
-
-
+      })
+    }
   });
 }
 //There are 3 classes for each of the 3 griw columns on large screens. This function calculates the
@@ -131,4 +131,4 @@ function calculateToggleClass(number) {
   }
 }
 setUpButtons();
-updateImages(1);
+updateImages(1, "75430200%40N04");
