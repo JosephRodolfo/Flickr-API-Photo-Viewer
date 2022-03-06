63ba2ac7b12ecfae50cf5bbafd972313b91f4566
JosephRodolfo <38168806+JosephRodolfo@users.noreply.github.com>
2022-03-05 20:26:59 -0500

    Add expand or shrink images on click functionality

--- app.js
+++ app.js
@@ -2,17 +2,18 @@ var apiKey = config.YOUR_API_KEY;
 
 async function getImageList() {
   const response = await fetch(
-    "https://www.flickr.com/services/rest/?method=flickr.people.getPhotos&api_key=" + apiKey + "&user_id=75430200%40N04&format=json&nojsoncallback=1"
+    "https://www.flickr.com/services/rest/?method=flickr.people.getPhotos&api_key=" +
+      apiKey +
+      "&user_id=75430200%40N04&format=json&nojsoncallback=1"
   );
   const flickrObjectResponse = await response.json();
   //   console.log(flickrObjectResponse.photos.photo.length);
-  let sliced = flickrObjectResponse.photos.photo.slice(0, 5);
+  let sliced = flickrObjectResponse.photos.photo.slice(0, 10);
   return sliced;
 }
 
 function createImgSrc(serverId, imageId, secret) {
-  let imageSrc =
-    `https://live.staticflickr.com/${serverId}/${imageId}_${secret}.jpg`;
+  let imageSrc = `https://live.staticflickr.com/${serverId}/${imageId}_${secret}.jpg`;
   return imageSrc;
 }
 
@@ -26,15 +27,53 @@ function createAndAppend(imgSrc) {
   imageContainer.appendChild(imgHolder);
 }
 
-getImageList().then((value) =>
-  value.forEach((element) => {
-    let tempSrc = createImgSrc(element.server, element.id, element.secret);
-    createAndAppend(tempSrc);
-  })
-).finally( ()=>
-    document.querySelectorAll('img').forEach(element=> element.addEventListener('click', function() {
+getImageList()
+  .then((value) =>
+    value.forEach((element) => {
+      let tempSrc = createImgSrc(element.server, element.id, element.secret);
+      createAndAppend(tempSrc);
+    })
+  )
+  .finally(() =>
+    document.querySelectorAll("img").forEach((element, index) =>
+      element.addEventListener("click", function () {
+        let t = calculateToggleClass(index) + 1;
 
-        this.style.setProperty('position', 'absolute')
-        ;
-    }
-    )));
+        console.log(t);
+
+        this.classList.toggle("overlay-img-style-" + t);
+      })
+    )
+  );
+function calculateToggleClass(number) {
+  let x = number;
+  if (x === 0) {
+    return 0;
+  } else if (x === 1) {
+    return 1;
+  } else if (x === 2) {
+    return 2;
+  } else {
+    return calculateToggleClass((x -= 3));
+  }
+}
+
+/* 0 1 2
+   3 4 5
+   6 7 8
+   9 10 11
+   12 13 14
+   15 16 17
+
+
+
+*/
+
+/*  function overlayDeploy() {
+
+    
+    let overlayContainer = document.querySelector(".overlay-container");
+    overlayContainer.classList.add('overlay-on');
+    //overlayContainer.classList.add("overlay-on");
+
+}*/
