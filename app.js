9da798e12d55f9088fbd889cb2c80e7c7d0facff
JosephRodolfo <38168806+JosephRodolfo@users.noreply.github.com>
2022-03-06 10:26:59 -0500

    Add page navigation via arrows and page numbers and basic responsiveness on small screens

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 var apiKey = config.YOUR_API_KEY;
 
+let currentPageNumber = {
+  page: 1,
+};
+
+//async function, takes the desired page number returns list of photos with information needed to create src to display them,
+// currently set to only 10, but remove slice
+//and it would be all photos per page, 100;
+
 async function getImageList(pageNumber) {
   const response = await fetch(
     "https://www.flickr.com/services/rest/?method=flickr.people.getPhotos&api_key=" +
       apiKey +
-      "&user_id=75430200%40N04&page=" + 
-      pageNumber + "&format=json&nojsoncallback=1"
+      "&user_id=75430200%40N04&page=" +
+      pageNumber +
+      "&format=json&nojsoncallback=1"
   );
   const flickrObjectResponse = await response.json();
   //   console.log(flickrObjectResponse.photos.photo.length);
+  printPageNumbers(flickrObjectResponse.photos.pages);
+
   let sliced = flickrObjectResponse.photos.photo.slice(0, 10);
   return sliced;
 }
@@ -28,28 +39,85 @@ function createAndAppend(imgSrc) {
   imageContainer.appendChild(imgHolder);
 }
 
-getImageList(1)
-  .then((value) =>
-    value.forEach((element) => {
-      let tempSrc = createImgSrc(element.server, element.id, element.secret);
-      createAndAppend(tempSrc);
-    })
-  ).catch(error => alert(
-   "Something went wrong! " + error))
-  .finally(() =>
-    document.querySelectorAll("img").forEach((element, index) =>
-      element.addEventListener("click", function () {
-        let t = calculateToggleClass(index) + 1;
+//function that is called when pressing arrow buttons or number buttons to select or move between pages.
+function updateImages(pageNumberInput) {
+  let gridImageContainer = document.getElementById("grid-image-container");
+  gridImageContainer.innerHTML = "";
 
-        console.log(t);
-
-        this.classList.toggle("overlay-img-style-" + t);
+  getImageList(pageNumberInput)
+    .then((value) =>
+      value.forEach((element) => {
+        let tempSrc = createImgSrc(element.server, element.id, element.secret);
+        createAndAppend(tempSrc);
       })
     )
-  );
+    .catch((error) => alert("Something went wrong! " + error))
+    .finally(() =>
+      document.querySelectorAll("section img").forEach((element, index) =>
+        element.addEventListener("click", function () {
+          let t = calculateToggleClass(index) + 1;
 
-  //There are 3 classes for each of the 3 griw columns on large screens. This function calculates the 
-  //row so the correct class is given to the event listener to attach. 
+          this.classList.toggle("overlay-img-style-" + t);
+        })
+      )
+    );
+}
+
+function printPageNumbers(numberOfPages) {
+  let numbersContainer = document.getElementById("page-numbers-container");
+  numbersContainer.innerHTML = "";
+  for (let i = 0; i < numberOfPages; i++) {
+    let tempNumberHolder = document.createElement("div");
+    tempNumberHolder.innerText = i;
+    tempNumberHolder.id = i;
+
+    tempNumberHolder.addEventListener("click", function () {
+      currentPageNumber.page = this.innerText;
+      updateImages(this.innerText);
+      console.log(this.innerText);
+    });
+
+    numbersContainer.appendChild(tempNumberHolder);
+  }
+}
+
+function setUpButtons() {
+  let buttons = document.querySelectorAll("button");
+
+  buttons.forEach((element) => {
+
+
+    if(element.id==="left-button"){
+    element.addEventListener("click", function () {
+
+      currentPageNumber.page--;
+      updateImages(currentPageNumber.page);
+  });} else {
+
+
+    
+    element.addEventListener("click", function () {
+
+
+      currentPageNumber.page++;
+      updateImages(currentPageNumber.page);
+
+
+    })
+
+
+
+
+
+    
+  }
+
+
+
+  });
+}
+//There are 3 classes for each of the 3 griw columns on large screens. This function calculates the
+//row so the correct class is given to the event listener to attach.
 function calculateToggleClass(number) {
   let x = number;
   if (x === 0) {
@@ -62,23 +130,5 @@ function calculateToggleClass(number) {
     return calculateToggleClass((x -= 3));
   }
 }
-
-/* 0 1 2
-   3 4 5
-   6 7 8
-   9 10 11
-   12 13 14
-   15 16 17
-
-
-
-*/
-
-/*  function overlayDeploy() {
-
-    
-    let overlayContainer = document.querySelector(".overlay-container");
-    overlayContainer.classList.add('overlay-on');
-    //overlayContainer.classList.add("overlay-on");
-
-}*/
+setUpButtons();
+updateImages(1);
